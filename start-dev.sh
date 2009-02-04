8de6b5d50330949529cfe8b2b6c617da538d870c
Jeremy Wall <jeremy@marzhillstudios.com>
2009-02-04 15:32:04 -0600

    added nitrogen dependency

--- start-dev.sh
+++ start-dev.sh
@@ -6,6 +6,7 @@ erl \
 	-name nitrogen@someplace.net \
 	-pa ./ebin -pa ./include \
 	-pa ./deps/mochiweb-src/ebin -pa ./deps/mochiweb-src/include \
+	-pa ./deps/nitrogen-src/ebin -pa ./deps/nitrogen-src/include \
 	-s make all \
     -s reloader \
 	-eval "application:start(iterate)"
--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ erl \
 	-name nitrogen@localhost \
 	-pa ./ebin -pa ./include \
 	-pa ./deps/mochiweb-src/ebin -pa ./deps/mochiweb-src/include \
+	-pa ./deps/nitrogen-src/ebin -pa ./deps/nitrogen-src/include \
 	-s make all \
 	-eval "application:start(iterate)"
