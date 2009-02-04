a0f578ea380078022a97cf4eb4557dbe612ca865
Jeremy Wall <jeremy@marzhillstudios.com>
2009-02-04 15:48:40 -0600

    change in runners

--- start-dev.sh
+++ start-dev.sh
@@ -5,8 +5,8 @@ echo Starting Nitrogen.
 erl \
 	-name nitrogen@someplace.net \
 	-pa ./ebin -pa ./include \
-	-pa ./deps/mochiweb-src/ebin -pa ./deps/mochiweb-src/include \
-	-pa ./deps/nitrogen-src/ebin -pa ./deps/nitrogen-src/include \
+	-pa ./deps/mochiweb/ebin -pa ./deps/mochiweb/include \
+	-pa ./deps/nitrogen/ebin -pa ./deps/nitrogen/include \
 	-s make all \
     -s reloader \
 	-eval "application:start(iterate)"
--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo Starting Nitrogen.
 erl \
 	-name nitrogen@localhost \
 	-pa ./ebin -pa ./include \
-	-pa ./deps/mochiweb-src/ebin -pa ./deps/mochiweb-src/include \
-	-pa ./deps/nitrogen-src/ebin -pa ./deps/nitrogen-src/include \
+	-pa ./deps/mochiweb/ebin -pa ./deps/mochiweb/include \
+	-pa ./deps/nitrogen/ebin -pa ./deps/nitrogen/include \
 	-s make all \
 	-eval "application:start(iterate)"
