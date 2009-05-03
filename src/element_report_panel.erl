8117dc81a850d9b429e0870209c6e1476d703535
Jeremy Wall <jeremy@marzhillstudios.com>
2009-05-02 23:37:12 -0500

    the start of a story point chart

--- src/element_report_panel.erl
+++ src/element_report_panel.erl
@@ -16,8 +16,11 @@ render() ->
 render(ControlId, _Record) ->
     PanelId = wf:temp_id()
     , CChart = build_completion_chart(wf_session:session(working_in))
+    , SpChart = build_sp_chart(wf_session:session(working_in))
     , Panel = #panel{id=PanelId, body=["Completion Burnup chart"
-        , CChart]}
+        , CChart
+        , "Story_Point Chart"
+        , SpChart]}
     , element_panel:render(ControlId, Panel)
 .
 
@@ -26,8 +29,6 @@ build_completion_chart(undefined) ->
 build_completion_chart({Type, Name}) ->
     TS = completion_for_last_week({Type, Name})
     % TODO(jwall): make this into a widget and blog about it
-    %, Length = length(TS)
-    %, Title = wf:f("~p Completion Burndown for Last 7 days", [Name])
     , Width = 300
     , Height = 150
     , MinX = date_util:date_to_epoch(date_util:subtract(date(), {days, 7})) * 1000 
@@ -44,17 +45,16 @@ build_completion_chart({Type, Name}) ->
 build_sp_chart(undefined) ->
     "no reports selected";
 build_sp_chart({Type, Name}) ->
-    TS = completion_for_last_week({Type, Name})
+    TS = iterate_report:stats(incomplete_sp, Type, Name)
+    , {Complete, Incomplete} = iterate_wf:iteration_story_points(Name)
     % TODO(jwall): make this into a widget and blog about it
-    %, Length = length(TS)
-    %, Title = wf:f("~p Completion Burndown for Last 7 days", [Name])
     , Width = 300
     , Height = 150
     , MinX = date_util:date_to_epoch(date_util:subtract(date(), {days, 7})) * 1000 
-    , MaxX = date_util:date_to_epoch(date_util:add(date(), {days, 1})) * 1000
+    , MaxX = date_util:date_to_epoch(date_util:add(date(), {days, 2})) * 1000
     , YTicks = 5
     , MinY = 0
-    , MaxY = 100
+    , MaxY = Complete + Incomplete
     , Data = {Name, [[V#tsentry.epoch, V#tsentry.value] || V <- TS]}
     , #flot_chart{width=Width, height=Height
         , minx=MinX, maxx=MaxX, yticks=YTicks, xticks=8
