5954337850af3e0f48297b05396e8cea382b3792
Jeremy Wall <jeremy@marzhillstudios.com>
2009-12-27 21:46:43 -0600

    updated dev script to use a static name instead of depending on the hostname

--- start-dev.sh
+++ start-dev.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 
 echo Starting Nitrogen.
 ERL_LIBS=deps erl \
-	-name nitrogen@$(hostname) \
+	-name nitrogen@localhost \
 	-pa ./ebin -pa ./include \
 	-s make all \
 	-s reloader \
